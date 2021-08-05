644899c1c4cd977e4a95fba48cee46d552dbd413
nvedant2008 <83070500+nvedant2008@users.noreply.github.com>
2021-08-05 17:53:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,7 +30,8 @@ function setup() {
 	var ball_options={
 		isStatic:false,
 		restitution:0.3,
-		density:0.4
+		density:1.2
+		friction:0
 	}
 
 	ball = Bodies.circle(260,100,radius/2.6,ball_options);
